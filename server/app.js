22de0f0714556ddce0ab3f64d3eacdca94fb04ad
Dhruv Jain <dhruvjain657@gmail.com>
2023-06-04 00:07:41 +0530

    Added login name phoneNo

--- server/app.js
+++ server/app.js
@@ -2,13 +2,32 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/add-user", (req, res) => {
+  var name = req.body.name;
+  var phoneNo = req.body.phoneNo;
+
+  console.log(name, phoneNo);
+});
+
+app.post("/admin-login", (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  console.log(email, password);
 });
 
 app.get("/about", (req, res) => {
   res.send("this is my about route");
 });
+
+
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
