3e658654f75c1611f515d87ee8ab9f566f1f99a7
ved142 <ved78142@gmail.com>
2023-06-05 20:41:52 +0530

    Cors Added

--- server/app.js
+++ server/app.js
@@ -8,13 +8,21 @@ app.use(express.urlencoded({ extended: true }));
 const User = require("./model/userSchema");
 const Admin = require("./model/adminSchema");
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 const PORT = 4421;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Home route!");
+app.get("/home", (req, res) => {
+  res.send("Home route is there!");
 });
 
 // to add user into database
@@ -47,22 +55,22 @@ app.post("/add-user", async (req, res) => {
 // to login into admin account
 app.post("/admin-login", async (req, res) => {
   const adminDetails = {
-    email: req.body.email,
+    email: req.body.username,
     password: req.body.password,
   };
-  // console.log(adminDetails);
+  console.log(adminDetails);
 
   const founduser = await Admin.findOne({ email: adminDetails.email });
-  // console.log(founduser);
+  console.log(founduser);
 
   if (founduser) {
     if (founduser.password === adminDetails.password) {
       res.send("authentication successful");
     } else {
-      res.send("wrong password entered");
+      res.status(400).send("wrong password entered");
     }
   } else {
-    res.send("user not found");
+    res.status(400).send("User was not registered , Not Found!!");
   }
 });
 
