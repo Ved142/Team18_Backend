e5c0b7d1ed26a9bf3b3df8851f41aeca87d3180f
ved142 <ved78142@gmail.com>
2023-06-06 17:14:48 +0530

    staff addition

--- server/app.js
+++ server/app.js
@@ -5,12 +5,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 3000;
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-const { UserAdd } = require('./Routes/Users/addUser')
-const { adminLogin } = require('./Routes/Admin/adminLogin')
-const { staffAdd } = require('./Routes/Staff/AddStaff')
-const { staffDelete } = require('./Routes/Staff/DeleteStaff')
+const PORT = 4421;
+
+const { UserAdd } = require("./Routes/Users/addUser");
+const { adminLogin } = require("./Routes/Admin/adminLogin");
+const { staffAdd } = require("./Routes/Staff/AddStaff");
+const { staffDelete } = require("./Routes/Staff/DeleteStaff");
+const { staffDetails } = require("./Routes/Staff/getStaffDetails");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -19,10 +28,11 @@ app.get("/", (req, res) => {
   res.send("Home route!");
 });
 
-app.use('/add-user', UserAdd);
-app.use('/admin-login', adminLogin);
-app.use('/add-Staff', staffAdd);
-app.use('/delete-Staff', staffDelete);
+app.use("/add-user", UserAdd);
+app.use("/admin-login", adminLogin);
+app.use("/add-Staff", staffAdd);
+app.use("/delete-Staff", staffDelete);
+app.use("/details-staff", staffDetails);
 
 app.get("/about", (req, res) => {
   res.send("this is my about route");
